9b68548291f65945420ce7da300422b4255d14fb
Dmitry Shilovsky <wppbbh@gmail.com>
2023-04-28 05:07:38 +0400

    добавил смену текста кнопки start/stop

--- app/src/main/java/com/example/test/HomeActivity.java
+++ app/src/main/java/com/example/test/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.test;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,6 +14,34 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button buttonInfo = findViewById(R.id.info);
+        Button buttonStartStop = findViewById(R.id.start);
+
+        buttonInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "info", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonStartStop.setOnClickListener(new View.OnClickListener() {
+            boolean isServiceRunning = false;
+            @Override
+            public void onClick(View v) {
+                    if (isServiceRunning) {
+                        Toast.makeText(HomeActivity.this, "stop", Toast.LENGTH_SHORT).show();
+                        stopService(new Intent(HomeActivity.this, MyForegroundService.class));
+                        buttonStartStop.setText("START");
+                    } else {
+                        Toast.makeText(HomeActivity.this, "start", Toast.LENGTH_SHORT).show();
+                        Intent foregroundServiceIntent = new Intent(HomeActivity.this, MyForegroundService.class);
+                        ContextCompat.startForegroundService(HomeActivity.this, foregroundServiceIntent);
+                        buttonStartStop.setText("STOP");
+                    }
+                    isServiceRunning = !isServiceRunning;
+            }
+        });
     }
 }
 
